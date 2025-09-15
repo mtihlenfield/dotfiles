d5c547fa40740fe222d05bcdc7a17441b648b387
Matt Ihlenfield <mtihlenfield@protonmail.com>
2025-09-15 08:01:27 -0400

    Updated neovim version

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@ install_manual_deps () {
     sudo mv hadolint-Linux-x86_64 /opt/hadolint
 
     # install nvim
-    wget -O /tmp/nvim.appimage 'https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.appimage'
+    wget -O /tmp/nvim.appimage 'https://github.com/neovim/neovim/releases/download/v0.11.4/nvim-linux-x86_64.appimage'
     chmod +x /tmp/nvim.appimage
     sudo mv /tmp/nvim.appimage "$NVIM_APPIMAGE"
 }
