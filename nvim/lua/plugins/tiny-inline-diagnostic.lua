15c92d0ec2b7722664cc0a77c68a52f650ba4430
Matt Ihlenfield <mtihlenfield@protonmail.com>
2026-06-26 08:30:46 -0400

    enable multiline in tiny-diagnostic

--- nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -3,7 +3,13 @@ return {
     event = "VeryLazy",
     priority = 1000,
     config = function()
-        require("tiny-inline-diagnostic").setup()
+        require("tiny-inline-diagnostic").setup({
+            options = {
+                multilines = {
+                    enabled = true,
+                },
+            },
+        })
         vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
     end,
 }
